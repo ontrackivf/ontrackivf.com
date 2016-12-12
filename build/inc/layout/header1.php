89e09775a90a6200177e20bac3f28ca2dc8b277d
Steven Smith <topshelfsmith@gmail.com>
2016-12-12 12:15:57 -0500

    setup code kit, added bourbon & neat

--- build/inc/layout/header1.php
+++ build/inc/layout/header1.php
@@ -3,6 +3,11 @@ echo '<!DOCTYPE html>
 <html>
 	<head>
 		<title>OnTrack™</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+		<meta name="viewport" content="width=device-width">
+		<meta name="viewport" content="initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" media="all" href="/css/webtests.css">
+		<script type="text/javascript" src="/js/webtests.js"></script>
 	</head>
 	<body>
 ';
