bac0b9a0c2a047ec1a1a528d6f7bd1f11c38e514
Steven Smith <topshelfsmith@gmail.com>
2016-12-12 15:59:31 -0500

    add web fonts

--- build/inc/layout/header1.php
+++ build/inc/layout/header1.php
@@ -8,6 +8,7 @@ echo '<!DOCTYPE html>
 		<meta name="viewport" content="initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" media="all" href="/css/webtests.css">
 		<script type="text/javascript" src="/js/webtests.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat|Source+Sans+Pro" rel="stylesheet">
 	</head>
 	<body>
 ';
