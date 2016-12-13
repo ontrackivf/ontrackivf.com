49c991ee84d6a7abb48b3a091bd4a207283f15e5
Steven Smith <topshelfsmith@gmail.com>
2016-12-13 10:56:29 -0500

    value prop1 and associated styles

--- build/inc/value_props/1.php
+++ build/inc/value_props/1.php
@@ -1,5 +1,16 @@
-<div class="holder">
-	
-	<img class="logo" src="/img/logo_white.png">
-	<p>Hello World.</p>
-</div>
+
+
+	<div class="holder">
+		<img class="logo" src="/img/logo_white.png">
+	</div>
+
+	<div class="holder">
+		<div class="text-content">
+			<h2 class="tagline">Fertility Treatment Doesn't Come With a Map</h2>
+			<p>OnTrack™ helps people manage their fertility medication by telling them when they need to take it, how many they have left, dosage changes, and when they need to refill them.</p>
+			<div class="button">Learn More</div>
+		</div>
+		<div class="image-content">
+			<img src="/img/macbook_iphone.png">
+		</div>
+	</div>
