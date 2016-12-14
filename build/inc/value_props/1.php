5800fda77c9309a89296e8553893cc098d5d6228
Steven Smith <topshelfsmith@gmail.com>
2016-12-14 13:44:19 -0500

    adjusted spacing added light background

--- build/inc/value_props/1.php
+++ build/inc/value_props/1.php
@@ -1,18 +1,22 @@
-<div class="holder">
-	<img class="logo" src="/img/logo_white.png">
-</div>
-<div class="holder">
-	<div class="text-content">
-		<h2 class="tagline">Why wonder? Know what to expect. Know you're OnTrack.</h2>
-		<p>Without OnTrack, 15 out of 100 people will make a scheduling or medication mistake during their IVF treatment. OnTrack puts you back in control and let's you know you're doing everything right.</p>
-		<div class="button" onclick="showModal('email_modal')">Learn More</div>
+<div class="background_container">
+	<div class="holder">
+		<img class="logo" src="/img/logo_white.png">
 	</div>
-	<div class="image-content">
-		<img src="/img/macbook_iphone.png">
+	<div class="holder">
+		<div class="text-content">
+			<h2 class="tagline">Why wonder? Know what to expect. Know you're OnTrack.</h2>
+			<p>Without OnTrack, 15 out of 100 people will make a scheduling or medication mistake during their IVF treatment. OnTrack puts you back in control and let's you know you're doing everything right.</p>
+			<div class="button" onclick="showModal('email_modal')">Learn More</div>
+		</div>
+		<div class="image-content">
+			<img src="/img/macbook_iphone.png">
+		</div>
 	</div>
 </div>
-<div class="holder white">
-
+<div class="background_container light_back">
+	<div class="holder">
+		<br><br><br><br><br><br><br><br><br><br><br><br>
+	</div>
 </div>
 
 
