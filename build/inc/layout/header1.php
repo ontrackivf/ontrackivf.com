9e77c4111e14d2debe51b79e6fa56efcf1a98f7b
Steven Smith <topshelfsmith@gmail.com>
2016-12-15 15:14:29 -0500

    added bing meta tag

--- build/inc/layout/header1.php
+++ build/inc/layout/header1.php
@@ -6,6 +6,7 @@ echo '<!DOCTYPE html>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 		<meta name="viewport" content="width=device-width">
 		<meta name="viewport" content="initial-scale=1.0">
+		<meta name="msvalidate.01" content="3866C8705AFD21C3256E5B5AC277315F" />
 		<link rel="stylesheet" type="text/css" media="all" href="/css/webtests.css">
 		<script type="text/javascript" src="/js/webtests.js"></script>
 		<link href="https://fonts.googleapis.com/css?family=Montserrat|Source+Sans+Pro" rel="stylesheet">
